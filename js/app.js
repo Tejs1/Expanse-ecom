96b6e675591cce4619f02f9f28c96eeb20ae0fc1
Tejs1 <67797070+Tejs1@users.noreply.github.com>
2022-02-22 01:30:00 +0530

    Revert "landing page addes"
    This reverts commit ac79d8414d333927fe150937bf710e9caea0b536.

--- js/app.js
+++
@@ -1,9 +0,0 @@
-var animation = bodymovin.loadAnimation({
-  // animationData: { /* ... */ },
-  container: document.getElementById("icon-container"), // required
-  path: "./js/data5.json", // required
-  renderer: "svg", // required
-  loop: true, // optional
-  autoplay: true, // optional
-  name: "Demo Animation", // optional
-});
